47c8ce56845df206eaa962c921c214431003c999
skes2525 <skes2525@gmail.com>
2021-06-14 10:22:46 +0800

    迴圈放child裡面 要如何多子程序?

--- lab6/lab6_mutex.cpp
+++ lab6/lab6_mutex.cpp
@@ -12,6 +12,7 @@ using namespace std;
 pthread_mutex_t mutex;
 
 int gpio_all;
+string inputdata;
 
 int gpio_export(unsigned int gpio){
     int fd, len;
@@ -75,54 +76,63 @@ int gpio_set_value(unsigned int gpio, int value){
     return 0;
 }
 
-void *setGPIO(string input, int i){
+void *setGPIO(void *argv){
     ///
     ///lock
     pthread_mutex_lock(&mutex);
-    ///
-    ///全域變數gpio_all會被lock
-    if(i==0){
-        gpio_all=396;
-    }else if(i==1){
-        gpio_all=255;
-    }else if(i==2){
-        gpio_all=428;
-    }else if(i==3){
-        gpio_all=427;
-    }
-    ///
-    ///決定好gpio_all之後，開啟led燈
-	if(input[i]=="1"){
-        gpio_set_value(gpio_all, 1);
-	}else if(input[i]=="0"){
-        gpio_set_value(gpio_all, 0);
+    int i=*(int*)argv;
+    for(int j=0; j<=3; j++){
+        ///
+        ///全域變數gpio_all會被lock
+        if(i==0){
+            gpio_all=396;
+        }else if(i==1){
+            gpio_all=255;
+        }else if(i==2){
+            gpio_all=428;
+        }else if(i==3){
+            gpio_all=427;
+        }
+        ///
+        ///決定好gpio_all之後，開啟led燈
+        if(inputdata[i]=='1'){
+            gpio_set_value(gpio_all, 1);
+        }else if(inputdata[i]=='0'){
+            gpio_set_value(gpio_all, 0);
+        }
+        i++;
     }
     ///
     ///unlock
+    pthread_mutex_unlock(&mutex);
     pthread_exit(NULL);
 }
 
-void *setGPIO_u(string input, int i){
+void *setGPIO_u(void *argv){
     ///
     ///lock
     pthread_mutex_lock(&mutex);
-    ///
-    ///全域變數gpio_all會被lock
-    if(i==0){
-        gpio_all=396;
-    }else if(i==1){
-        gpio_all=255;
-    }else if(i==2){
-        gpio_all=428;
-    }else if(i==3){
-        gpio_all=427;
-    }
-    ///
-    ///決定好gpio_all之後，開啟led燈
-	if(input[i]=="0"){
-        gpio_set_value(gpio_all, 1);
-	}else if(input[i]=="1"){
-        gpio_set_value(gpio_all, 0);
+    int i=*(int*)argv;
+    for(int j=0; j<=3; j++){
+        ///
+        ///全域變數gpio_all會被lock
+        if(i==0){
+            gpio_all=396;
+        }else if(i==1){
+            gpio_all=255;
+        }else if(i==2){
+            gpio_all=428;
+        }else if(i==3){
+            gpio_all=427;
+        }
+        ///
+        ///決定好gpio_all之後，開啟led燈
+        if(inputdata[i]=='0'){
+            gpio_set_value(gpio_all, 1);
+        }else if(inputdata[i]=='1'){
+            gpio_set_value(gpio_all, 0);
+        }
+        i++;
     }
     ///
     ///unlock
@@ -133,34 +143,39 @@ void *setGPIO_u(string input, int i){
 
 int main(int argc, char *argv[]){
     pthread_mutex_init(&mutex, 0);                  //初始化mutex
-    pthread_t thread1, thread2, thread3, thread4;   //建立子程序
+    pthread_t thread1;   //建立子程序
     ///
     ///first, export all gpio
     gpio_export(396);
     gpio_export(255);
     gpio_export(428);
     gpio_export(427);
+    gpio_set_dir(396, "out");
+    gpio_set_dir(255, "out");
+    gpio_set_dir(428, "out");
+    gpio_set_dir(427, "out");
 
     ///
     ///start to shine
     string input1=argv[1], input2=argv[2];
+    inputdata=input1;
     int time=2*stoi(input2);
-    for(time; time>=0; time--){
+    int i=0;
+    for(time; time>0; time--){
         if(time%2==0){
-            pthread_create(&thread1, NULL, setGPIO, input1, 0);
-            pthread_create(&thread2, NULL, setGPIO, input1, 1);
-            pthread_create(&thread3, NULL, setGPIO, input1, 2);
-            pthread_create(&thread4, NULL, setGPIO, input1, 3);
+            pthread_create(&thread1, NULL, setGPIO, (void*)&i);
         }else{
-            pthread_create(&thread1, NULL, setGPIO_u, input1, 0);
-            pthread_create(&thread2, NULL, setGPIO_u, input1, 1);
-            pthread_create(&thread3, NULL, setGPIO_u, input1, 2);
-            pthread_create(&thread4, NULL, setGPIO_u, input1, 3);
+            pthread_create(&thread1, NULL, setGPIO_u, (void*)&i);
         }
-        sleep(300);
+        sleep(1);
     }
     pthread_mutex_destroy(&mutex);
-
+    ///
+    ///turn off the lights
+    gpio_set_value(396, 0);
+    gpio_set_value(255, 0);
+    gpio_set_value(428, 0);
+    gpio_set_value(427, 0);
     ///
     ///last, export all gpio
     gpio_unexport(396);
